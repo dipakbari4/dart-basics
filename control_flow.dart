b694095a93a40096b8460f631314b2b0962ec759
Dipak Bari <dipakbari4@gmail.com>
2020-08-01 02:12:04 +0530

    Control flow

---
+++ control_flow.dart
@@ -0,0 +1,33 @@
+var name = "Dipak Bari";
+var dob = 1993;
+var bodyTemp = 98.5;
+var hobbies = ['Songs', 'Movies', 'Books'];
+var profileImage = {
+  'tags': ['profile'],
+  'url': 'path/to/profile.jpg'
+};
+
+main(List<String> args) {
+  // Conditional statement
+  if (dob >= 2001) {
+    print('Born in 21st century');
+  } else if (dob >= 1901) {
+    print('Born in 20th century');
+  }
+
+  // enhanced for loop
+  for (var hobby in hobbies) {
+    print(hobby);
+  }
+
+  // simple for loop
+  for (var month = 0; month <= 12; month++) {
+    print(month);
+  }
+
+  // while loop
+  while (dob < 2020) {
+    dob += 1;
+    print('Birthday $dob');
+  }
+}
