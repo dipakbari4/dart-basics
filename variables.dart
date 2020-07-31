d98957d88b43b53b128bf0874e9e90e78bce3165
Dipak Bari <dipakbari4@gmail.com>
2020-08-01 02:00:14 +0530

    Variables demo

---
+++ variables.dart
@@ -0,0 +1,16 @@
+var name = "Dipak Bari";
+var dob = 1993;
+var bodyTemp = 98.5;
+var hobbies = ['Songs', 'Movies', 'Books'];
+var profileImage = {
+  'tags': ['profile'],
+  'url': 'path/to/profile.jpg'
+};
+
+main(List<String> args) {
+  print(name);
+  print(dob);
+  print(bodyTemp);
+  print(hobbies);
+  print(profileImage);
+}
